343def910cdc8d44a98cde32dfe1156a3647a714
Mark Tyrkba <awfulmarco@gmail.com>
2025-08-12 22:02:14 +0300

    Few remaps

--- lua/rakivo/init.lua
+++ lua/rakivo/init.lua
@@ -150,8 +150,9 @@ vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
 vim.opt.showtabline = 0
 vim.opt.completeopt = { "menuone", "noinsert", "noselect" }
-vim.cmd.colorscheme("custom")
+-- vim.cmd.colorscheme("custom")
 -- vim.cmd.colorscheme("habamax")
+vim.cmd.colorscheme("naysayer")
 
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
